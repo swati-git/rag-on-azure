0e0b1f9f4d522681064368a8e5371ad981770a5a
space <swati.phartiyal@gmail.com>
2026-04-28 10:46:41 +0530

    Correct the spelling

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account_rag" {
-  name                     = "${var.environment}-${var.region}-storage_account-rag"
+  name                     = "documentstore"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
