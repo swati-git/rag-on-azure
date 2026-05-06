31f2cac28c0fefd53ae58ffe973558dd4db34d88
space <swati.phartiyal@gmail.com>
2026-05-06 13:02:32 +0530

    Add output variables to function module so that they can be accessed by other modules

--- infra/modules/functions/outputs.tf
+++ infra/modules/functions/outputs.tf
@@ -1,3 +1,17 @@
 output "webhook_function_url" {
   value = "https://${azurerm_linux_function_app.webhook_receiver.default_hostname}/api/webhook_receiver"
 }
+
+output "service_plan_id" {
+  value = azurerm_service_plan.main.id
+}
+
+
+output "storage_account_name" {
+  value = azurerm_storage_account.functions.name
+}
+
+output "storage_account_key" {
+  value = azurerm_storage_account.functions.primary_access_key
+  sensitive = true
+}
